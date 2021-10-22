fa6e92266b59ff75a806f28b05e676551fd304fc
DESKTOP-FISKTM0\nikik <niki.kachelmaier@gmail.com>
2021-10-22 17:27:21 +0200

    modulo neg

--- stackCalculator/src/main/java/at/edu/c02/calculator/logic/CalculatorImpl.java
+++ stackCalculator/src/main/java/at/edu/c02/calculator/logic/CalculatorImpl.java
@@ -13,6 +13,8 @@ public class CalculatorImpl implements Calculator {
 	@Override
 	public double perform(Operation op) throws CalculatorException {
 
+
+
 		double b = pop();
 		double a = pop();
 
@@ -30,7 +32,13 @@ public class CalculatorImpl implements Calculator {
 			return a * b;
 
 		case mod:
-			return a % b;
+			if (a < 0) {
+				return floatModfirst(a, b);
+			} else if (b < 0) {
+				return floatModsecond(a, b);
+			}
+			else return floatModfirst(a,b);
+
 		}
 		return 0;
 	}
@@ -50,6 +58,16 @@ public class CalculatorImpl implements Calculator {
 	@Override
 	public void clear() {
 		stack_.clear();
+	}
+
+	double floatModfirst(double x, double y){
+		// x mod y behaving the same way as Math.floorMod but with doubles
+		return (x - Math.floor(x/y) * y);
+	}
+
+	double floatModsecond(double y, double x){
+		// x mod y behaving the same way as Math.floorMod but with doubles
+		return (y - Math.floor(y/x) * x);
 	}
 
 }
